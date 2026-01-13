9a28548818759f7ee30961994c7d9bed98c057e5
김연호 <kimfine000@gmail.com>
2026-01-13 10:25:20 +0900

    feat: MissionQuestionJpaMapper 구현

--- src/main/java/com/process/clash/adapter/persistence/roadmap/missionquestion/MissionQuestionJpaMapper.java
+++ src/main/java/com/process/clash/adapter/persistence/roadmap/missionquestion/MissionQuestionJpaMapper.java
@@ -1,6 +1,7 @@
 package com.process.clash.adapter.persistence.roadmap.missionquestion;
 
 import com.process.clash.adapter.persistence.roadmap.choice.ChoiceJpaMapper;
+import com.process.clash.adapter.persistence.roadmap.mission.MissionJpaRepository;
 import com.process.clash.domain.roadmap.MissionQuestion;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
@@ -10,12 +11,25 @@ import org.springframework.stereotype.Component;
 public class MissionQuestionJpaMapper {
 
     private final ChoiceJpaMapper choiceJpaMapper;
+    private final MissionJpaRepository missionJpaRepository;
 
     public MissionQuestionJpaEntity toJpaEntity(MissionQuestion question) {
-        return null;
+        return new MissionQuestionJpaEntity(
+                question.getId(),
+                missionJpaRepository.getReferenceById(question.getMissionId()),
+                question.getContent(),
+                question.getExplanation(),
+                question.getChoices().stream().map(choiceJpaMapper::toJpaEntity).toList()
+        );
     }
 
     public MissionQuestion toDomain(MissionQuestionJpaEntity entity) {
-        return null;
+        return new MissionQuestion(
+                entity.getId(),
+                entity.getMission().getId(),
+                entity.getContent(),
+                entity.getExplanation(),
+                entity.getChoices().stream().map(choiceJpaMapper::toDomain).toList()
+        );
     }
 }
