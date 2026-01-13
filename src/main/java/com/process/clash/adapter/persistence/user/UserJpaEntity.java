96990ff7bcfabe1960894fa7c46fe9b27efd10e2
황정빈 <hjbin1211@gmail.com>
2026-01-13 10:55:47 +0900

    feat: Add UserJpaEnti8ty and User

--- src/main/java/com/process/clash/adapter/persistence/user/UserJpaEntity.java
+++ src/main/java/com/process/clash/adapter/persistence/user/UserJpaEntity.java
@@ -5,6 +5,10 @@ import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Table(name = "users")
@@ -15,6 +19,38 @@ public class UserJpaEntity {
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private String username;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private Boolean ableToAddRival;
+
     @Enumerated(EnumType.STRING)
     private Major major;
+
+    public UserJpaEntity create(String username, String name, String password) {
+        UserJpaEntity userJpaEntity = new UserJpaEntity();
+
+        userJpaEntity.username = username;
+        userJpaEntity.name = name;
+        userJpaEntity.password = password;
+        userJpaEntity.ableToAddRival = true;
+        userJpaEntity.major = null;
+
+        return userJpaEntity;
+    }
 }
