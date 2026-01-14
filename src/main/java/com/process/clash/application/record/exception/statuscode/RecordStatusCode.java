390fef497ba8bc24e37e6b9f5287752b91abef5d
daehyeong2 <baconbacon1231@gmail.com>
2026-01-14 22:35:00 +0900

    refactor: record 상태코드 패키지 정리 및 공부 세션 상태코드 추가

--- src/main/java/com/process/clash/application/record/exception/statuscode/RecordStatusCode.java
+++ src/main/java/com/process/clash/application/record/exception/statuscode/RecordStatusCode.java
@@ -1,4 +1,4 @@
-package com.process.clash.application.record.exception.status;
+package com.process.clash.application.record.exception.statuscode;
 
 import com.process.clash.application.common.exception.statuscode.ErrorCategory;
 import com.process.clash.application.common.exception.statuscode.StatusCode;
@@ -13,6 +13,11 @@ public enum RecordStatusCode implements StatusCode {
         "TASK_NOT_FOUND",
         "과목을 찾을 수 없습니다.",
         ErrorCategory.NOT_FOUND
+    ),
+    STUDY_SESSION_ALREADY_STARTED(
+        "STUDY_SESSION_ALREADY_STARTED",
+        "이미 공부 세션이 시작되었습니다.",
+        ErrorCategory.CONFLICT
     );
 
     private final String code;
