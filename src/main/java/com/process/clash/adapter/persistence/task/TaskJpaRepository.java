8603a238182766dfb91521612bc580787b6f48ce
daehyeong2 <baconbacon1231@gmail.com>
2026-01-14 15:56:30 +0900

    fix: N+1 문제 해결

--- src/main/java/com/process/clash/adapter/persistence/task/TaskJpaRepository.java
+++ src/main/java/com/process/clash/adapter/persistence/task/TaskJpaRepository.java
@@ -1,11 +1,13 @@
 package com.process.clash.adapter.persistence.task;
 
 import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TaskJpaRepository extends JpaRepository<TaskJpaEntity, Long> {
 
+    @EntityGraph(attributePaths = "user")
     List<TaskJpaEntity> findAllByUserId(Long userId);
 }
