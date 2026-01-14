6853513149e18c157d8abe24856ca0d0dcd492ff
황정빈 <hjbin1211@gmail.com>
2026-01-14 16:27:38 +0900

    fix: AuthUser 호출 부분 제거 및 @AuthenticatedActor로 대치

--- src/main/java/com/process/clash/adapter/web/mainpage/controller/MainPageController.java
+++ src/main/java/com/process/clash/adapter/web/mainpage/controller/MainPageController.java
@@ -4,6 +4,7 @@ import com.process.clash.adapter.web.common.ApiResponse;
 import com.process.clash.adapter.web.mainpage.dto.GetCompareWithYesterdayDto;
 import com.process.clash.adapter.web.mainpage.dto.GetMyRivalActingDto;
 import com.process.clash.adapter.web.mainpage.dto.GetUserProfileDto;
+import com.process.clash.adapter.web.security.AuthenticatedActor;
 import com.process.clash.application.common.actor.Actor;
 import com.process.clash.application.mainpage.data.GetCompareWithYesterdayData;
 import com.process.clash.application.mainpage.data.GetMyRivalActingData;
@@ -11,9 +12,7 @@ import com.process.clash.application.mainpage.data.GetUserProfileData;
 import com.process.clash.application.mainpage.port.in.GetCompareWithYesterdayUseCase;
 import com.process.clash.application.mainpage.port.in.GetMyRivalActingUseCase;
 import com.process.clash.application.mainpage.port.in.GetUserProfileUseCase;
-import com.process.clash.infrastructure.principle.AuthUser;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -30,10 +29,9 @@ public class MainPageController {
     // 유저 정보 조회 - 상단바 프로필 정보
     @GetMapping("/user-info")
     public ApiResponse<GetUserProfileDto.Response> getUserProfile(
-            @AuthenticationPrincipal AuthUser authUser
+            @AuthenticatedActor Actor actor
     ) {
 
-        Actor actor = authUser.toActor();
         GetUserProfileData.Command command = GetUserProfileData.Command.from(actor);
         GetUserProfileData.Result result = getUserProfileUseCase.execute(command);
         GetUserProfileDto.Response response = GetUserProfileDto.Response.from(result);
@@ -44,10 +42,9 @@ public class MainPageController {
     // TODO: 추가 구현 필요합니다
     @GetMapping("/compare")
     public ApiResponse<GetCompareWithYesterdayDto.Response> getCompareWithYesterday(
-            @AuthenticationPrincipal AuthUser authUser
+            @AuthenticatedActor Actor actor
     ) {
 
-        Actor actor = authUser.toActor();
         GetCompareWithYesterdayData.Command command = GetCompareWithYesterdayData.Command.from(actor);
         GetCompareWithYesterdayData.Result result = getCompareWithYesterdayUseCase.execute(command);
         GetCompareWithYesterdayDto.Response response = GetCompareWithYesterdayDto.Response.from(result);
@@ -58,10 +55,9 @@ public class MainPageController {
     // TODO: 추가 구현 필요합니다
     @GetMapping("/my-rivals")
     public ApiResponse<GetMyRivalActingDto.Response> getMyRivalActing(
-            @AuthenticationPrincipal AuthUser authUser
+            @AuthenticatedActor Actor actor
     ) {
 
-        Actor actor = authUser.toActor();
         GetMyRivalActingData.Command command = GetMyRivalActingData.Command.from(actor);
         GetMyRivalActingData.Result result = getMyRivalActingUseCase.execute(command);
         GetMyRivalActingDto.Response response = GetMyRivalActingDto.Response.from(result);
