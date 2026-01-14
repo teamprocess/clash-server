a1387510e742d6177ecbcf55d429a736dc6eed68
chaeyn <chaulfe@gmail.com>
2026-01-14 10:15:27 +0900

    feat: 상품, 시즌 엔티티

---
+++ src/main/java/com/process/clash/adapter/persistence/shop/season/SeasonJpaEntity.java
@@ -0,0 +1,43 @@
+package com.process.clash.adapter.persistence.shop.season;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "seasons")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class SeasonJpaEntity {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @UpdateTimestamp
+    @Column(nullable = false)
+    private LocalDateTime updatedAt;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private Date startDate;
+
+    @Column(nullable = false)
+    private Date endDate;
+
+    @Column(nullable = false)
+    private Boolean isActive;
+}
---
+++ src/main/java/com/process/clash/domain/shop/enums/ProductGoodsType.java
@@ -0,0 +1,6 @@
+package com.process.clash.domain.shop.enums;
+
+public enum ProductGoodsType {
+    COOKIE,
+    TOKEN
+}
