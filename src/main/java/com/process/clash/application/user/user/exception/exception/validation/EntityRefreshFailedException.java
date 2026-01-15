cadd03778959df692caaa5361cea8a7f994a9d78
김연호 <kimfine000@gmail.com>
2026-01-15 15:29:26 +0900

    fix: 실수로 안 올린 예외 구현

---
+++ src/main/java/com/process/clash/application/user/user/exception/exception/validation/EntityRefreshFailedException.java
@@ -0,0 +1,13 @@
+package com.process.clash.application.user.user.exception.exception.validation;
+
+import com.process.clash.application.common.exception.exception.ValidationException;
+
+public class EntityRefreshFailedException extends ValidationException {
+    public EntityRefreshFailedException() {
+        super();
+    }
+
+    public EntityRefreshFailedException(Throwable cause) {
+        super(cause);
+    }
+}
---
+++ src/main/java/com/process/clash/application/user/user/exception/exception/validation/ServletContextUnavailableException.java
@@ -0,0 +1,13 @@
+package com.process.clash.application.user.user.exception.exception.validation;
+
+import com.process.clash.application.common.exception.exception.ValidationException;
+
+public class ServletContextUnavailableException extends ValidationException {
+    public ServletContextUnavailableException() {
+        super();
+    }
+
+    public ServletContextUnavailableException(Throwable cause) {
+        super(cause);
+    }
+}
