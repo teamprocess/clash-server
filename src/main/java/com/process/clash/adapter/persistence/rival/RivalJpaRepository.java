077e1a88b6437f2d092280d722f15a81be2823ee
황정빈 <hjbin1211@gmail.com>
2026-01-15 20:26:28 +0900

    fix: RivalJpaRepository 쿼리 수정

--- src/main/java/com/process/clash/adapter/persistence/rival/RivalJpaRepository.java
+++ src/main/java/com/process/clash/adapter/persistence/rival/RivalJpaRepository.java
@@ -1,11 +1,30 @@
 package com.process.clash.adapter.persistence.rival;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface RivalJpaRepository extends JpaRepository<RivalJpaEntity, Long> {
 
-    int countAllByMy_Id(Long myId);
-    int countAllByOpponent_Id(Long opponentId);
+    @Query("""
+        select count(r)
+        from RivalJpaEntity as r
+        where r.my.id = :myId
+            and r.rivalLinkingStatus = 'ACCEPTED'
+    """)
+    int countAllByMy_Id(
+            @Param("myId") Long myId
+    );
+
+    @Query("""
+        select count(r)
+        from RivalJpaEntity as r
+        where r.opponent.id = :opponentId
+            and r.rivalLinkingStatus = 'ACCEPTED'
+    """)
+    int countAllByOpponent_Id(
+            @Param("opponentId") Long opponentId
+    );
 }
