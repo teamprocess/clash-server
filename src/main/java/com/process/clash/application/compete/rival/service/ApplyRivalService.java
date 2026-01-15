5021832fa02d0d3b7c94a254344af116d5678103
황정빈 <hjbin1211@gmail.com>
2026-01-15 20:43:15 +0900

    feat: ApplyRivalService 로직 구성

--- src/main/java/com/process/clash/application/compete/rival/service/ApplyRivalService.java
+++ src/main/java/com/process/clash/application/compete/rival/service/ApplyRivalService.java
@@ -3,6 +3,8 @@ package com.process.clash.application.compete.rival.service;
 import com.process.clash.application.compete.rival.data.ApplyRivalData;
 import com.process.clash.application.compete.rival.policy.ApplyRivalPolicy;
 import com.process.clash.application.compete.rival.port.in.ApplyRivalUseCase;
+import com.process.clash.application.compete.rival.port.out.RivalRepositoryPort;
+import com.process.clash.domain.rival.entity.Rival;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 public class ApplyRivalService implements ApplyRivalUseCase {
 
     private final ApplyRivalPolicy applyRivalPolicy;
+    private final RivalRepositoryPort rivalRepositoryPort;
 
     @Override
     @Transactional
@@ -19,6 +22,10 @@ public class ApplyRivalService implements ApplyRivalUseCase {
 
         applyRivalPolicy.check(command);
 
+        for (int i = 0; i < command.ids().size(); i++) {
+            Rival rival = Rival.createDefault(command.actor().id(), command.ids().get(i).id());
 
+            rivalRepositoryPort.save(rival);
+        }
     }
 }
