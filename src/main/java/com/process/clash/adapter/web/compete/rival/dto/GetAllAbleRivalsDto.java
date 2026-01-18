134ad124617d7dc3419a3d9be3afaf371806c9b6
황정빈 <hjbin1211@gmail.com>
2026-01-18 16:13:45 +0900

    fix: Rival 현재 상황 저장 로직 삭제

--- src/main/java/com/process/clash/adapter/web/compete/rival/dto/GetAllAbleRivalsDto.java
+++ src/main/java/com/process/clash/adapter/web/compete/rival/dto/GetAllAbleRivalsDto.java
@@ -20,7 +20,8 @@ public class GetAllAbleRivalsDto {
                             .map(data -> new UserInfo(
                                     data.id(),
                                     data.name(),
-                                    data.githubId()
+                                    data.githubId(),
+                                    data.profileImage()
                             ))
                     .toList()
             );
@@ -30,6 +31,7 @@ public class GetAllAbleRivalsDto {
     private record UserInfo(
             Long id,
             String name,
-            String githubId
+            String githubId,
+            String profileImage
     ) {}
 }
