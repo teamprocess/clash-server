9f657198e68e821d34fd70146fe1921ced47c953
황정빈 <hjbin1211@gmail.com>
2026-01-19 14:03:57 +0900

    fix: FindAbleRival 로직 수정

--- src/main/java/com/process/clash/adapter/persistence/user/usergithub/UserGitHubJpaRepository.java
+++ src/main/java/com/process/clash/adapter/persistence/user/usergithub/UserGitHubJpaRepository.java
@@ -1,7 +1,9 @@
 package com.process.clash.adapter.persistence.user.usergithub;
 
+import com.process.clash.application.compete.rival.data.AbleRivalInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -13,4 +15,19 @@ public interface UserGitHubJpaRepository extends JpaRepository<UserGitHubJpaEnti
 
     @Query("select ug from UserGitHubJpaEntity ug join fetch ug.user where ug.gitHubId is not null")
     List<UserGitHubJpaEntity> findAllWithUserAndGitHubId();
+
+    @Query("""
+        select new com.process.clash.application.compete.rival.data.AbleRivalInfo(
+            u.id,
+            u.name,
+            ug.gitHubId,
+            u.profileImage
+        )
+        from UserGitHubJpaEntity ug
+        join ug.user u
+        where u.id not in :excludedUserIds
+    """)
+    List<AbleRivalInfo> findAbleRivalsWithUserInfo(
+            @Param("excludedUserIds") List<Long> excludedUserIds
+    );
 }
