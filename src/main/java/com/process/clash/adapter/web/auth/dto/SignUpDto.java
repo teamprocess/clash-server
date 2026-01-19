f99b73c9249c697966b68f711b99c2813b72335c
김연호 <kimfine000@gmail.com>
2026-01-19 22:44:32 +0900

    feat: 비밀번호 제약 추가

--- src/main/java/com/process/clash/adapter/web/auth/dto/SignUpDto.java
+++ src/main/java/com/process/clash/adapter/web/auth/dto/SignUpDto.java
@@ -3,6 +3,8 @@ package com.process.clash.adapter.web.auth.dto;
 import com.process.clash.application.user.user.data.SignUpData;
 import jakarta.validation.constraints.NotBlank;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public class SignUpDto {
     @Schema(name = "SignUpDtoRequest")
@@ -12,6 +14,11 @@ public class SignUpDto {
             @NotBlank(message = "이메일은 필수 입력값입니다.")
             String email,
             @NotBlank(message = "비밀번호는 필수 입력값입니다.")
+            @Size(min = 8, max = 20, message = "비밀번호는 8자 이상 20자 이하로 입력해주세요.")
+            @Pattern(
+                    regexp = "^(?=.*[A-Za-z])(?=.*\\d).+$",
+                    message = "비밀번호는 영문과 숫자를 포함해야 합니다."
+            )
             String password,
             @NotBlank(message = "이름은 필수 입력값입니다.")
             String name
