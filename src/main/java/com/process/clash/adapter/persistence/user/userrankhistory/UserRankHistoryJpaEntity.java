744896e67c80dc60bf8b6b7db5c429c5ce1925a6
황정빈 <hjbin1211@gmail.com>
2026-01-20 18:34:14 +0900

    feat: UserRankHistoryJpaEntity 추가

---
+++ src/main/java/com/process/clash/adapter/persistence/user/userrankhistory/UserRankHistoryJpaEntity.java
@@ -0,0 +1,53 @@
+package com.process.clash.adapter.persistence.user.userrankhistory;
+
+import com.process.clash.adapter.persistence.shop.season.SeasonJpaEntity;
+import com.process.clash.adapter.persistence.user.user.UserJpaEntity;
+import com.process.clash.domain.user.userrankhistory.enums.ExpTier;
+import com.process.clash.domain.user.userrankhistory.enums.RankTier;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "user_rank_history")
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class UserRankHistoryJpaEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @CreationTimestamp
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @Column(nullable = false)
+    private int rank;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private ExpTier expTier;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private RankTier rankTier;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fk_user_id", nullable = false)
+    private UserJpaEntity user;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "fk_season_id", nullable = false)
+    private SeasonJpaEntity season;
+}
