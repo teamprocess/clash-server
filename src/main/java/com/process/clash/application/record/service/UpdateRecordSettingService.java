4b9f8f929a2fb571f38be42d2a904b3989347acd
황정빈 <hjbin1211@gmail.com>
2026-01-20 15:10:09 +0900

    fix: UpdateRecordSettingService 로직 수정

--- src/main/java/com/process/clash/application/record/service/UpdateRecordSettingService.java
+++ src/main/java/com/process/clash/application/record/service/UpdateRecordSettingService.java
@@ -2,9 +2,8 @@ package com.process.clash.application.record.service;
 
 import com.process.clash.application.record.dto.UpdateRecordSettingData;
 import com.process.clash.application.record.port.in.UpdateRecordSettingUseCase;
-import com.process.clash.application.user.user.exception.exception.notfound.UserNotFoundException;
+import com.process.clash.application.user.userpomodorosetting.exception.exception.notfound.UserPomodoroSettingNotFoundException;
 import com.process.clash.application.user.userpomodorosetting.port.out.UserPomodoroSettingRepositoryPort;
-import com.process.clash.domain.user.user.entity.User;
 import com.process.clash.domain.user.userpomodorosetting.entity.UserPomodoroSetting;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
@@ -20,20 +19,20 @@ public class UpdateRecordSettingService implements UpdateRecordSettingUseCase {
     @Override
     public UpdateRecordSettingData.Result execute(UpdateRecordSettingData.Command command) {
         UserPomodoroSetting userPomodoroSetting = userPomodoroSettingRepositoryPort.findByUserId(command.actor().id())
-            .orElseThrow(UserNotFoundException::new);
+            .orElseThrow(UserPomodoroSettingNotFoundException::new);
 
-        User updatedUser = user.withPomodoroSettings(
+        UserPomodoroSetting updatedUserPomodoroSetting = userPomodoroSetting.updatePomodoroSetting(
                 command.pomodoroEnabled(),
                 command.studyMinute(),
                 command.breakMinute()
         );
 
-        User savedUser = userRepositoryPort.save(updatedUser);
+        UserPomodoroSetting saveddUserPomodoroSetting = userPomodoroSettingRepositoryPort.save(updatedUserPomodoroSetting);
 
         return UpdateRecordSettingData.Result.create(
-            savedUser.pomodoroEnabled(),
-            savedUser.pomodoroStudyMinute(),
-            savedUser.pomodoroBreakMinute()
+                saveddUserPomodoroSetting.pomodoroEnabled(),
+                saveddUserPomodoroSetting.pomodoroStudyMinute(),
+                saveddUserPomodoroSetting.pomodoroBreakMinute()
         );
     }
 }
