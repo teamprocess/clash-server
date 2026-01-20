b5a2b18b2087799aee1e7e095094d2bc4bb25187
황정빈 <hjbin1211@gmail.com>
2026-01-20 15:20:14 +0900

    fix: GetRecordSettingService 로직 수정

--- src/main/java/com/process/clash/application/record/service/GetRecordSettingService.java
+++ src/main/java/com/process/clash/application/record/service/GetRecordSettingService.java
@@ -2,9 +2,9 @@ package com.process.clash.application.record.service;
 
 import com.process.clash.application.record.dto.GetRecordSettingData;
 import com.process.clash.application.record.port.in.GetRecordSettingUseCase;
-import com.process.clash.application.user.user.exception.exception.notfound.UserNotFoundException;
-import com.process.clash.application.user.user.port.out.UserRepositoryPort;
-import com.process.clash.domain.user.user.entity.User;
+import com.process.clash.application.user.userpomodorosetting.exception.exception.notfound.UserPomodoroSettingNotFoundException;
+import com.process.clash.application.user.userpomodorosetting.port.out.UserPomodoroSettingRepositoryPort;
+import com.process.clash.domain.user.userpomodorosetting.entity.UserPomodoroSetting;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +12,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class GetRecordSettingService implements GetRecordSettingUseCase {
 
-    private final UserRepositoryPort userRepositoryPort;
+    private final UserPomodoroSettingRepositoryPort userPomodoroSettingRepositoryPort;
 
     @Override
     public GetRecordSettingData.Result execute(GetRecordSettingData.Command command) {
-        User user = userRepositoryPort.findById(command.actor().id())
-            .orElseThrow(UserNotFoundException::new);
+        UserPomodoroSetting userPomodoroSetting = userPomodoroSettingRepositoryPort.findByUserId(command.actor().id())
+            .orElseThrow(UserPomodoroSettingNotFoundException::new);
 
         return GetRecordSettingData.Result.create(
-            user.pomodoroEnabled(),
-            user.pomodoroStudyMinute(),
-            user.pomodoroBreakMinute()
+                userPomodoroSetting.pomodoroEnabled(),
+                userPomodoroSetting.pomodoroStudyMinute(),
+                userPomodoroSetting.pomodoroBreakMinute()
         );
     }
 }
