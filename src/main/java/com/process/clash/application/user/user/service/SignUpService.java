500e556ab42a394bfeedfa3689d317dda70bf740
김연호 <kimfine000@gmail.com>
2026-01-21 17:20:27 +0900

    comment: 이해 안 되던 정답 제출 읽으면서 주석 달기

--- src/main/java/com/process/clash/application/user/user/service/SignUpService.java
+++ src/main/java/com/process/clash/application/user/user/service/SignUpService.java
@@ -1,6 +1,10 @@
 package com.process.clash.application.user.user.service;
 
 import com.process.clash.application.common.util.TokenGenerator;
+import com.process.clash.application.mail.exception.exception.InvalidMailException;
+import com.process.clash.application.mail.exception.exception.MailDeliveryException;
+import com.process.clash.application.mail.exception.exception.MailMessageCreationException;
+import com.process.clash.application.mail.exception.exception.MailServerAuthenticationException;
 import com.process.clash.application.mail.port.out.SendVerificationEmailPort;
 import com.process.clash.application.mail.port.out.VerificationCodePort;
 import com.process.clash.application.user.user.data.SignUpData;
@@ -33,6 +37,26 @@ public class SignUpService implements SignUpUseCase {
 	private static final long VERIFICATION_CODE_EXPIRATION_MS = 5 * 60 * 1000L; // 인증 코드 만료: 5분
 	private static final long PENDING_USER_EXPIRATION_MS = 30 * 60 * 1000L; // PENDING USER 만료: 30분(추후 정보 유지 + 코드 재전송 가능 예정)
 
+	/**
+	 * 회원가입 요청을 처리하고 이메일 인증 세션을 식별하기 위한 토큰을 반환합니다.
+	 * * <p><b>상세 프로세스:</b></p>
+	 * <ul>
+	 * <li>중복된 사용자 이름 또는 이메일이 있는지 DB에서 확인합니다.</li>
+	 * <li>비밀번호를 암호화하여 {@link User} 엔티티를 생성하고 캐시에 임시 보관합니다.</li>
+	 * <li>6자리의 난수 인증 코드를 생성하여 저장하고, 해당 메일로 발송합니다.</li>
+	 * </ul>
+	 *
+	 * @param command 회원가입에 필요한 정보를 담은 명령 객체
+	 * @return 이메일 인증에 필요한 토큰
+	 * @throws UsernameAlreadyExistException 이미 존재하는 username으로 요청한 경우
+	 * @throws EmailAlreadyExistException 이미 존재하는 email로 요청한 경우
+	 * @throws MailMessageCreationException 메일 메세지 생성 중 오류가 난 경우
+	 * @throws MailServerAuthenticationException 메일 서버 인증을 실패한 경우
+	 * @throws InvalidMailException 메일 형식이 잘못된 경우
+	 * @throws MailDeliveryException 메일 발송 중 서버와 통신 오류가 난 경우
+	 *
+	 * @author 김연호
+	 */
 	@Override
 	@Transactional
 	public String execute(SignUpData.Command command) {
