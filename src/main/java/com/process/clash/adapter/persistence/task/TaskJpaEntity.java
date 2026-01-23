84911c2c63426cb3e3f9e9a9aca730cbd3e968b1
chaeyn <chaulfe@gmail.com>
2026-01-23 14:00:30 +0900

    delete: 사용하지 않는 import

--- src/main/java/com/process/clash/adapter/persistence/task/TaskJpaEntity.java
+++ src/main/java/com/process/clash/adapter/persistence/task/TaskJpaEntity.java
@@ -3,8 +3,6 @@ package com.process.clash.adapter.persistence.task;
 import com.process.clash.adapter.persistence.user.user.UserJpaEntity;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
