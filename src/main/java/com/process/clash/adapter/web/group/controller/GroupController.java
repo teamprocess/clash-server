75c44364b43c46c21d997866ed43e7538913d342
daehyeong2 <baconbacon1231@gmail.com>
2026-01-24 15:30:11 +0900

    feat: group web 패키지 구조 생성

---
+++ src/main/java/com/process/clash/adapter/web/group/controller/GroupController.java
@@ -0,0 +1,15 @@
+package com.process.clash.adapter.web.group.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/groups")
+@RequiredArgsConstructor
+public class GroupController {
+
+    // private final
+
+    // @GetMapping(...
+}
