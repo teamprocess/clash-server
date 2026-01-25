11c5946acafe97001945a0ad6d5a04bbb1d8aec0
황정빈 <hjbin1211@gmail.com>
2026-01-25 22:37:30 +0900

    hotfix: 409로 수정 및 다른 로직 수정

--- src/main/java/com/process/clash/application/compete/rival/battle/exception/exception/badrequest/AlreadyInBattleException.java
+++ src/main/java/com/process/clash/application/compete/rival/battle/exception/exception/badrequest/AlreadyInBattleException.java
@@ -1,9 +1,9 @@
 package com.process.clash.application.compete.rival.battle.exception.exception.badrequest;
 
-import com.process.clash.application.common.exception.exception.BadRequestException;
+import com.process.clash.application.common.exception.exception.ConflictException;
 import com.process.clash.application.compete.rival.battle.exception.statuscode.BattleStatusCode;
 
-public class AlreadyInBattleException extends BadRequestException {
+public class AlreadyInBattleException extends ConflictException {
     public AlreadyInBattleException() {
         super(BattleStatusCode.ALREADY_IN_BATTLE);
     }
