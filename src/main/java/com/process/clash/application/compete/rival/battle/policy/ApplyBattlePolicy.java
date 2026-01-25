d908bfd5ebad1b724d5ecdb69b0fb05a180ba344
황정빈 <hjbin1211@gmail.com>
2026-01-25 22:00:55 +0900

    hotfix: 배틀 신청 policy 추가

---
+++ src/main/java/com/process/clash/application/compete/rival/battle/policy/ApplyBattlePolicy.java
@@ -0,0 +1,20 @@
+package com.process.clash.application.compete.rival.battle.policy;
+
+import com.process.clash.application.compete.rival.battle.exception.exception.badrequest.AlreadyInBattleException;
+import com.process.clash.application.compete.rival.battle.port.out.BattleRepositoryPort;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class ApplyBattlePolicy {
+
+    private final BattleRepositoryPort battleRepositoryPort;
+
+    public void check(Long id) {
+
+        if (battleRepositoryPort.existsActiveBattleByRivalId(id)) {
+            throw new AlreadyInBattleException();
+        }
+    }
+}
