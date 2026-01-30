e65914635b9022575c5a79d336bac1a4be2d85af
daehyeong2 <baconbacon1231@gmail.com>
2026-01-31 04:10:55 +0900

    feat: GitHub OAuth 클라이언트 설정 및 어댑터 추가

--- src/main/java/com/process/clash/adapter/github/GithubGraphqlAdapter.java
+++ src/main/java/com/process/clash/adapter/github/GithubGraphqlAdapter.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 import org.springframework.web.reactive.function.client.WebClientRequestException;
@@ -38,6 +39,7 @@ public class GithubGraphqlAdapter implements GithubStatsFetchPort {
     private static final DateTimeFormatter INSTANT_FORMATTER = DateTimeFormatter.ISO_INSTANT;
     private static final int PAGE_SIZE = 100;
 
+    @Qualifier("githubWebClient")
     private final WebClient githubWebClient;
     private final ObjectMapper objectMapper;
     private final GithubGraphqlQueries queries;
