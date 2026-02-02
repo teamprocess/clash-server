6935d1d0a9b9f392f43baacd3b40ccac0c18fc7a
김연호 <kimfine000@gmail.com>
2026-02-02 14:11:39 +0900

    feat: swagger에서 csrf 토큰 활성화

--- src/main/java/com/process/clash/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/process/clash/infrastructure/config/OpenApiConfig.java
@@ -1,7 +1,10 @@
 package com.process.clash.infrastructure.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.context.annotation.Configuration;
 
@@ -14,6 +17,13 @@ import org.springframework.context.annotation.Configuration;
         servers = {
                 @Server(url = "https://api.clash.kr", description = "운영 서버 (HTTPS)"),
         }
+)
+@SecurityScheme(
+        name = "CSRF",
+        type = SecuritySchemeType.APIKEY,
+        in = SecuritySchemeIn.HEADER,
+        paramName = "X-XSRF-TOKEN",
+        description = "CSRF 토큰 (쿠키의 XSRF-TOKEN 값을 입력하세요)"
 )
 @Configuration
 public class OpenApiConfig {
