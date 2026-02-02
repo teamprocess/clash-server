19e781f17363452b668e65855d342ae10cfa90a4
김연호 <kimfine000@gmail.com>
2026-02-02 14:21:10 +0900

    hotfix: swagger에서 csrf 토큰 활성화

--- src/main/java/com/process/clash/infrastructure/config/OpenApiConfig.java
+++ src/main/java/com/process/clash/infrastructure/config/OpenApiConfig.java
@@ -1,30 +1,31 @@
 package com.process.clash.infrastructure.config;
 
-import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
-import io.swagger.v3.oas.annotations.servers.Server;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-@OpenAPIDefinition(
-        info = @Info(
-                title = "Clash API",
-                version = "v1",
-                description = "Clash 서버 API 문서"
-        ),
-        servers = {
-                @Server(url = "https://api.clash.kr", description = "운영 서버 (HTTPS)"),
-        }
-)
-@SecurityScheme(
-        name = "CSRF",
-        type = SecuritySchemeType.APIKEY,
-        in = SecuritySchemeIn.HEADER,
-        paramName = "X-XSRF-TOKEN",
-        description = "CSRF 토큰 (쿠키의 XSRF-TOKEN 값을 입력하세요)"
-)
 @Configuration
 public class OpenApiConfig {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Clash API")
+                        .version("v1")
+                        .description("Clash 서버 API 문서"))
+                .addServersItem(new io.swagger.v3.oas.models.servers.Server()
+                        .url("https://api.clash.kr")
+                        .description("운영 서버 (HTTPS)"))
+                .components(new Components()
+                        .addSecuritySchemes("CSRF", new SecurityScheme()
+                                .type(SecurityScheme.Type.APIKEY)
+                                .in(SecurityScheme.In.HEADER)
+                                .name("X-XSRF-TOKEN")
+                                .description("CSRF 토큰 (쿠키의 XSRF-TOKEN 값을 입력하세요)")))
+                .addSecurityItem(new SecurityRequirement().addList("CSRF"));
+    }
 }
