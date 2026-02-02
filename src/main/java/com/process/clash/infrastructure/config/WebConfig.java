8eb846c10ea031afe11ed8308d6047aa5677336b
김연호 <kimfine000@gmail.com>
2026-02-02 11:40:19 +0900

    feat: rest template 타임아웃 설정

--- src/main/java/com/process/clash/infrastructure/config/WebConfig.java
+++ src/main/java/com/process/clash/infrastructure/config/WebConfig.java
@@ -2,13 +2,21 @@ package com.process.clash.infrastructure.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.client.SimpleClientHttpRequestFactory;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class WebConfig {
 
+    private final static int CONNECT_TIMEOUT_MILLISECOND = 5000;
+    private final static int READ_TIMEOUT_MILLISECOND = 10000;
+
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplate();
+        SimpleClientHttpRequestFactory factory = new SimpleClientHttpRequestFactory();
+        factory.setConnectTimeout(CONNECT_TIMEOUT_MILLISECOND);
+        factory.setReadTimeout(READ_TIMEOUT_MILLISECOND);
+
+        return new RestTemplate(factory);
     }
 }
