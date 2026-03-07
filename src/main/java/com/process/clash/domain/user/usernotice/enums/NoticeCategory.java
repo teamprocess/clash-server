b901a1b823a4ea90abf98a4d85786a20b8ee719e
황정빈 <hjbin1211@gmail.com>
2026-03-08 02:25:12 +0900

    refactor: 미사용 NoticeCategory SHOW_* 값 제거

--- src/main/java/com/process/clash/domain/user/usernotice/enums/NoticeCategory.java
+++ src/main/java/com/process/clash/domain/user/usernotice/enums/NoticeCategory.java
@@ -8,10 +8,6 @@ public enum NoticeCategory {
     APPLY_BATTLE("상대방이 배틀을 신청했어요.", true),
     ACCEPT_BATTLE("상대방이 배틀을 수락했어요.", false),
     REJECT_BATTLE("상대방이 배틀을 거절했어요.", false),
-    SHOW_ACCEPT_RIVAL("상대방의 라이벌 신청을 수락했어요.", false),
-    SHOW_REJECT_RIVAL("상대방의 라이벌 신청을 거절했어요.", false),
-    SHOW_ACCEPT_BATTLE("상대방의 배틀 신청을 수락했어요.", false),
-    SHOW_REJECT_BATTLE("상대방의 배틀 신청을 거절했어요.", false),
     GLOBAL_NOTICE("전체 공지사항이 도착했어요.", false);
 
     private final String message;
