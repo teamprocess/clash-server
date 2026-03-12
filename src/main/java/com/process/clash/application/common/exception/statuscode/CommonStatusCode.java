edf71edd2f1279f2c038379bd07334e9917996bd
redjungi <redjungi09@gmail.com>
2026-03-12 11:56:19 +0900

    refactor: common StatusCode 및 GlobalExceptionHandler HttpStatus 적용

--- src/main/java/com/process/clash/application/common/exception/statuscode/CommonStatusCode.java
+++ src/main/java/com/process/clash/application/common/exception/statuscode/CommonStatusCode.java
@@ -2,6 +2,7 @@ package com.process.clash.application.common.exception.statuscode;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @RequiredArgsConstructor
@@ -10,58 +11,58 @@ public enum CommonStatusCode implements StatusCode {
     ENTITY_REFRESH_FAILED(
             "ENTITY_REFRESH_FAILED",
             "엔티티 새로고침에 실패했습니다.",
-            ErrorCategory.INTERNAL_ERROR
+            HttpStatus.INTERNAL_SERVER_ERROR
     ),
 
     SERVLET_CONTEXT_UNAVAILABLE(
             "SERVLET_CONTEXT_UNAVAILABLE",
             "서블릿 컨텍스트를 사용할 수 없습니다.",
-            ErrorCategory.INTERNAL_ERROR
+            HttpStatus.INTERNAL_SERVER_ERROR
     ),
 
     INVALID_ARGUMENT(
             "INVALID_ARGUMENT",
             "유효하지 않은 인자입니다.",
-            ErrorCategory.BAD_REQUEST
+            HttpStatus.BAD_REQUEST
     ),
 
     ENDPOINT_NOT_FOUND(
             "ENDPOINT_NOT_FOUND",
             "존재하지 않는 엔드포인트입니다.",
-            ErrorCategory.NOT_FOUND
+            HttpStatus.NOT_FOUND
     ),
 
     METHOD_NOT_ALLOWED(
             "METHOD_NOT_ALLOWED",
             "지원하지 않는 HTTP 메서드입니다.",
-            ErrorCategory.METHOD_NOT_ALLOWED
+            HttpStatus.METHOD_NOT_ALLOWED
     ),
 
     ENDPOINT_MOVED(
             "ENDPOINT_MOVED",
             "엔드포인트가 변경되었습니다.",
-            ErrorCategory.GONE
+            HttpStatus.GONE
     ),
 
     UNAUTHORIZED(
             "UNAUTHORIZED",
             "인증이 필요한 서비스입니다.", // common으로 쓰기 모호한 느낌의 메세지
-            ErrorCategory.UNAUTHORIZED
+            HttpStatus.UNAUTHORIZED
     ),
 
     PERMISSION_DENIED(
             "PERMISSION_DENIED",
             "해당 작업을 수행할 권한이 없습니다.",
-            ErrorCategory.FORBIDDEN
+            HttpStatus.FORBIDDEN
     ),
 
     INTERNAL_SERVER_ERROR(
             "INTERNAL_SERVER_ERROR",
             "서버에 오류가 발생했습니다.",
-            ErrorCategory.INTERNAL_ERROR
+            HttpStatus.INTERNAL_SERVER_ERROR
     );
 
     private final String code;
     private final String message;
-    private final ErrorCategory errorCategory;
+    private final HttpStatus httpStatus;
 }
