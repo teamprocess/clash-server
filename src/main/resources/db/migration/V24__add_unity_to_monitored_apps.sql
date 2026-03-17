fec2f87395d4161be414fc5e5c4edb36f341ac51
chaeyn <chaulfe@gmail.com>
2026-03-17 23:13:58 +0900

    feat: 유니티 추가

---
+++ src/main/resources/db/migration/V24__add_unity_to_monitored_apps.sql
@@ -0,0 +1,88 @@
+ALTER TABLE record_sessions
+    DROP CONSTRAINT IF EXISTS ck_record_sessions_app_id;
+
+ALTER TABLE record_sessions
+    ADD CONSTRAINT ck_record_sessions_app_id
+        CHECK (
+            app_id IS NULL
+                OR app_id IN (
+                    'VSCODE',
+                    'WEBSTORM',
+                    'INTELLIJ_IDEA',
+                    'PYCHARM',
+                    'GOLAND',
+                    'PHPSTORM',
+                    'RUBYMINE',
+                    'CLION',
+                    'RIDER',
+                    'ANDROID_STUDIO',
+                    'XCODE',
+                    'UNITY'
+                )
+        );
+
+ALTER TABLE record_session_segments
+    DROP CONSTRAINT IF EXISTS ck_record_session_segments_app_id;
+
+ALTER TABLE record_session_segments
+    ADD CONSTRAINT ck_record_session_segments_app_id
+        CHECK (
+            app_id IN (
+                'VSCODE',
+                'WEBSTORM',
+                'INTELLIJ_IDEA',
+                'PYCHARM',
+                'GOLAND',
+                'PHPSTORM',
+                'RUBYMINE',
+                'CLION',
+                'RIDER',
+                'ANDROID_STUDIO',
+                'XCODE',
+                'UNITY'
+            )
+        );
+
+ALTER TABLE record_develop_sessions_v2
+    DROP CONSTRAINT IF EXISTS ck_record_develop_sessions_v2_app_id;
+
+ALTER TABLE record_develop_sessions_v2
+    ADD CONSTRAINT ck_record_develop_sessions_v2_app_id
+        CHECK (
+            app_id IN (
+                'VSCODE',
+                'WEBSTORM',
+                'INTELLIJ_IDEA',
+                'PYCHARM',
+                'GOLAND',
+                'PHPSTORM',
+                'RUBYMINE',
+                'CLION',
+                'RIDER',
+                'ANDROID_STUDIO',
+                'XCODE',
+                'UNITY'
+            )
+        );
+
+ALTER TABLE record_develop_session_segments_v2
+    DROP CONSTRAINT IF EXISTS ck_record_develop_session_segments_v2_app_id;
+
+ALTER TABLE record_develop_session_segments_v2
+    ADD CONSTRAINT ck_record_develop_session_segments_v2_app_id
+        CHECK (
+            app_id IN (
+                'VSCODE',
+                'WEBSTORM',
+                'INTELLIJ_IDEA',
+                'PYCHARM',
+                'GOLAND',
+                'PHPSTORM',
+                'RUBYMINE',
+                'CLION',
+                'RIDER',
+                'ANDROID_STUDIO',
+                'XCODE',
+                'UNITY'
+            )
+        );
