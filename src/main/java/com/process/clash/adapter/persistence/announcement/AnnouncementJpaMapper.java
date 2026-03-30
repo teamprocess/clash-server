81ac1597074fd1ada12a6076fd872b5aab60bac5
redjungi <redjungi09@gmail.com>
2026-03-30 09:28:04 +0900

    feat: announcement테이블 추가 및 entity생성

---
+++ src/main/java/com/process/clash/adapter/persistence/announcement/AnnouncementJpaMapper.java
@@ -0,0 +1,23 @@
+package com.process.clash.adapter.persistence.announcement;
+
+import com.process.clash.domain.announcement.entity.Announcement;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AnnouncementJpaMapper {
+
+    public Announcement toDomain(AnnouncementJpaEntity entity) {
+        return new Announcement(
+                entity.getId(),
+                entity.getCreatedAt(),
+                entity.getUpdatedAt(),
+                entity.getTitle(),
+                entity.getAuthor(),
+                entity.getUserId(),
+                entity.getContent(),
+                entity.getStartAt(),
+                entity.getEndAt(),
+                entity.isHideable()
+        );
+    }
+}
