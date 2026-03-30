c7db056c3d94a42f1eacfa3bd59624558b321972
redjungi <redjungi09@gmail.com>
2026-03-30 09:38:16 +0900

    feat: 활성 공지 목록 조회 api생성

---
+++ src/main/java/com/process/clash/adapter/web/announcement/controller/AnnouncementController.java
@@ -0,0 +1,25 @@
+package com.process.clash.adapter.web.announcement.controller;
+
+import com.process.clash.adapter.web.announcement.dto.GetActiveAnnouncementsDto;
+import com.process.clash.adapter.web.common.ApiResponse;
+import com.process.clash.application.announcement.data.GetActiveAnnouncementsData;
+import com.process.clash.application.announcement.port.in.GetActiveAnnouncementsUseCase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/announcements")
+@RequiredArgsConstructor
+public class AnnouncementController {
+
+    private final GetActiveAnnouncementsUseCase getActiveAnnouncementsUseCase;
+
+    @GetMapping("/active")
+    public ApiResponse<GetActiveAnnouncementsDto.Response> getActiveAnnouncements() {
+        GetActiveAnnouncementsData.Result result = getActiveAnnouncementsUseCase.execute();
+        GetActiveAnnouncementsDto.Response response = GetActiveAnnouncementsDto.Response.from(result);
+        return ApiResponse.success(response, "활성 공지 목록을 성공적으로 조회했습니다.");
+    }
+}
