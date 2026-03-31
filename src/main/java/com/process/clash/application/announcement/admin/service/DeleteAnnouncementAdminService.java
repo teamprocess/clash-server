f73fbee91fd35051474246eeafb04ab5b18e712b
redjungi <redjungi09@gmail.com>
2026-03-31 11:42:46 +0900

    feat: 공지 어드민 삭제 service 구현

---
+++ src/main/java/com/process/clash/application/announcement/admin/service/DeleteAnnouncementAdminService.java
@@ -0,0 +1,30 @@
+package com.process.clash.application.announcement.admin.service;
+
+import com.process.clash.application.announcement.admin.data.DeleteAnnouncementAdminData;
+import com.process.clash.application.announcement.admin.port.in.DeleteAnnouncementAdminUseCase;
+import com.process.clash.application.announcement.exception.exception.notfound.AnnouncementNotFoundException;
+import com.process.clash.application.announcement.port.out.AnnouncementRepositoryPort;
+import com.process.clash.application.common.policy.CheckAdminPolicy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class DeleteAnnouncementAdminService implements DeleteAnnouncementAdminUseCase {
+
+    private final AnnouncementRepositoryPort announcementRepositoryPort;
+    private final CheckAdminPolicy checkAdminPolicy;
+
+    @Override
+    public void execute(DeleteAnnouncementAdminData.Command command) {
+        checkAdminPolicy.check(command.actor());
+
+        if (announcementRepositoryPort.findById(command.id()).isEmpty()) {
+            throw new AnnouncementNotFoundException();
+        }
+
+        announcementRepositoryPort.deleteById(command.id());
+    }
+}
