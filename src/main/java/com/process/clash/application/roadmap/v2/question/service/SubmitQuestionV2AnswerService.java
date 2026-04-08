ca6a80e5696b85fe242d4583dfc0db5ae3f15a01
김연호 <kimfine000@gmail.com>
2026-04-08 18:10:18 +0900

    feat: section clear 지급 쿠키 변경

--- src/main/java/com/process/clash/application/roadmap/v2/question/service/SubmitQuestionV2AnswerService.java
+++ src/main/java/com/process/clash/application/roadmap/v2/question/service/SubmitQuestionV2AnswerService.java
@@ -35,7 +35,7 @@ import java.util.Optional;
 public class SubmitQuestionV2AnswerService implements SubmitQuestionV2AnswerUseCase {
 
     private static final int CHAPTER_CLEAR_COOKIE_REWARD = 300;
-    private static final int SECTION_CLEAR_COOKIE_REWARD = 3000;
+    private static final int SECTION_CLEAR_COOKIE_REWARD = 1000;
 
     private final QuestionV2RepositoryPort questionV2RepositoryPort;
     private final ChapterV2RepositoryPort chapterV2RepositoryPort;
