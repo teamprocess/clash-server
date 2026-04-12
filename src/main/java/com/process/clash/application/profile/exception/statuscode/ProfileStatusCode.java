27b4cf694684ba0270c15012862dcf419d659d37
redjungi <redjungi09@gmail.com>
2026-04-12 15:07:29 +0900

    feat: 비공개 프로필 접근 예외(ProfilePrivatedException) 추가

--- src/main/java/com/process/clash/application/profile/exception/statuscode/ProfileStatusCode.java
+++ src/main/java/com/process/clash/application/profile/exception/statuscode/ProfileStatusCode.java
@@ -11,7 +11,8 @@ public enum ProfileStatusCode implements StatusCode {
 
     INVALID_PERIOD_CATEGORY("INVALID_PERIOD_CATEGORY", "잘못된 PeriodCategory입니다.", HttpStatus.BAD_REQUEST),
     ITEM_NOT_OWNED("ITEM_NOT_OWNED", "보유하지 않은 아이템입니다.", HttpStatus.FORBIDDEN),
-    INVALID_EQUIPPABLE_ITEM_CATEGORY("INVALID_EQUIPPABLE_ITEM_CATEGORY", "장착 가능한 아이템 카테고리가 아닙니다.", HttpStatus.BAD_REQUEST);
+    INVALID_EQUIPPABLE_ITEM_CATEGORY("INVALID_EQUIPPABLE_ITEM_CATEGORY", "장착 가능한 아이템 카테고리가 아닙니다.", HttpStatus.BAD_REQUEST),
+    PROFILE_PRIVATED("PROFILE_PRIVATED", "비공개 프로필입니다.", HttpStatus.FORBIDDEN);
 
     private final String code;
     private final String message;
