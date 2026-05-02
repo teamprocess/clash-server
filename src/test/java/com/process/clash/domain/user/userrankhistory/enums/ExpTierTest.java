e7795e31332bd272382ee042daeb9d134f2bc423
chaeyn <chaulfe@gmail.com>
2026-05-02 20:09:37 +0900

    feat: 랭킹 티어 기준 변경

--- src/test/java/com/process/clash/domain/user/userrankhistory/enums/ExpTierTest.java
+++ src/test/java/com/process/clash/domain/user/userrankhistory/enums/ExpTierTest.java
@@ -8,14 +8,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 class ExpTierTest {
 
     @Test
-    @DisplayName("exp가 100000 이상이면 DIAMOND다")
-    void fromExp_overOrEqual100k_returnsDiamond() {
-        assertThat(ExpTier.fromExp(100_000)).isEqualTo(ExpTier.DIAMOND);
+    @DisplayName("exp가 50000 이상이면 DIAMOND다")
+    void fromExp_overOrEqual50k_returnsDiamond() {
+        assertThat(ExpTier.fromExp(50_000)).isEqualTo(ExpTier.DIAMOND);
     }
 
     @Test
-    @DisplayName("exp가 99999 이하이면 GOLD다")
-    void fromExp_under100k_returnsGold() {
-        assertThat(ExpTier.fromExp(99_999)).isEqualTo(ExpTier.GOLD);
+    @DisplayName("exp가 49999 이하이면 GOLD다")
+    void fromExp_under50k_returnsGold() {
+        assertThat(ExpTier.fromExp(49_999)).isEqualTo(ExpTier.GOLD);
     }
 }
